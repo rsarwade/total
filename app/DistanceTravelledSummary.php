88e6257b99f593e8c4a199f97c2e318dd1f331a8
Rajdatta Sarwade <rajdattatestemail@gmail.com>
2018-01-11 13:39:44 +0530

    First commit putting all files in git for total project

---
+++ app/DistanceTravelledSummary.php
@@ -0,0 +1,12 @@
+<?php
+
+  namespace App;
+
+  use Illuminate\Database\Eloquent\Model;
+
+
+  class DistanceTravelledSummary extends Model {
+    public $timestamps = false;
+    protected $primaryKey = 'travel_id';
+
+  }
